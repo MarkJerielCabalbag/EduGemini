446b43a64b2ab231a30ba4869e5e27b1d58fba73
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-08-31 00:23:06 +0800

    allow origin

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,17 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+  next();
+});
+
 app.use(cookieParser());
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
