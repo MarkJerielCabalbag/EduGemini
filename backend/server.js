1eb345a5d21a777d72707d81762f021169549c8b
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-08-30 10:27:20 +0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,18 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({ origin: true, credentials: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
