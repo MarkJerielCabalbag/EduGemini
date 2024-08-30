b5cd0f64de812f0187d2888e251484160c87bbf6
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-08-30 09:24:36 +0800

    auth

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -5,10 +5,13 @@ import User from "../models/userModel.js";
 const protectRoutes = asyncHandler(async (req, res, next) => {
   let token;
 
-  token = req.cookies.jwt;
-
-  if (token) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
+      token = req.headers.authorization.split(" ")[1];
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.userId).select("-user_password");
