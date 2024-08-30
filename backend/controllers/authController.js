f1ffe87f9c08ab5540299b712b665df1c7fc27f3
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-08-30 23:48:44 +0800

    token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -116,11 +116,11 @@ const logoutUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user._id).select("-user_password");
 
-  if (user) {
-    generateToken(res, user._id);
-    return res.status(200).send(user);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
   }
-  return res.status(404).json({ message: "User not found" });
+  generateToken(res, user._id);
+  return res.status(200).send(user);
 });
 
 // //@desc     Update User Profile
