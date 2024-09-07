e0143f532648d79cdc27b43b8f665800b9861815
MarkJerielCabalbag <154601617+MarkJerielCabalbag@users.noreply.github.com>
2024-09-07 20:16:50 +0800

    Delete frontend directory

--- frontend/src/content/Video.jsx
+++
@@ -1,7 +0,0 @@
-// components/Video.jsx
-export default function Video({ src, className }) {
-  const html = `<video autoPlay muted loop><source src=${src} /></video>`;
-  return (
-    <div dangerouslySetInnerHTML={{ __html: html }} className={className} />
-  );
-}
--- frontend/src/utils/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-
-  return userInfo ? <Outlet /> : <Navigate to="/" replace />;
-};
-
-export default PrivateRoute;
