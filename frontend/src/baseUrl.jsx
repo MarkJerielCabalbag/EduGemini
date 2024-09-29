ccf79b7ece8ea1d7da6686df6578d788a9a5f4ce
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-09-29 23:31:42 +0800

    vercel deployment test

--- frontend/src/baseUrl.jsx
+++ frontend/src/baseUrl.jsx
@@ -1,4 +1,5 @@
-export const baseUrl = "https://edugemini.onrender.com";
+export const baseUrl = "https://edu-gemini-es6l.vercel.app/";
 // "https://edugemini.onrender.com";
 
 // "http://localhost:3000";
+// https://edu-gemini-es6l.vercel.app/
